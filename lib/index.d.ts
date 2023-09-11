918ba0330f1afacf10a400eac541bb0701f1de4f
max <maxguthmann09@gmail.com>
2023-09-11 18:38:15 +0200

    Added JSDoc

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,37 @@
+/**
+ * FireAPI - A simple wrapper for the 24Fire API
+ */
 export default class FireAPI {
+    /**
+     * The API key used for the requests
+     */
     private readonly apiKey;
+    /**
+     * @param apiKey The API key used for the requests
+     */
     constructor(apiKey: string);
+    /**
+     * @returns All available list endpoints
+     */
     list(): {
         vm: () => Promise<unknown>;
         hosts: () => Promise<unknown>;
         os: () => Promise<unknown>;
         iso: () => Promise<unknown>;
     };
+    /**
+     *
+     * @param vmid The ID of the VM
+     * @returns All available endpoints for DDoS protection
+     */
     ddos(vmid: string): {
         get: () => Promise<unknown>;
         edit: (layer4?: string, layer7?: string, ip_address?: string) => Promise<unknown>;
     };
+    /**
+     * @param vmid The ID of the VM
+     * @returns All available endpoints for backup management
+     */
     backup(vmid: string): {
         get: () => Promise<unknown>;
         create: () => Promise<unknown>;
@@ -19,16 +40,23 @@ export default class FireAPI {
         restore_status: (backup_id: string) => Promise<unknown>;
         remove: (backup_id: string) => Promise<unknown>;
     };
+    /**
+     * @param vmid The ID of the VM
+     * @returns All available endpoints for ISO management
+     */
     iso(vmid: string): {
         attach: (iso: "3cx_debian_10." | "arch_linux_23_08" | "centos_7" | "centos_9" | "fedora_38" | "proxmox_8" | "proxmox_backup_server_3" | "rocky_linux_9" | "system_rescue_cd") => Promise<unknown>;
         detach: () => Promise<unknown>;
     };
+    /**
+     * @returns All available endpoints for the VM
+     */
     vm(): {
         reinstall: (vmid: string, os?: "debian_11" | "debian_12" | "ubuntu_22_10" | "ubuntu_22_04" | "centos_7" | "windows_server_2019") => Promise<unknown>;
         create: (cores: number, mem: number, disk: number, os: "debian_11" | "debian_12" | "ubuntu_22_10" | "ubuntu_22_04" | "centos_7" | "windows_server_2019", hostsystem: "nl_xeon" | "nl_ryzen", ips?: number, backup_slots?: number, network_speed?: number) => Promise<unknown>;
         config: (vmid: string) => Promise<{
-            edit: (vmid: string, cores?: number, mem?: number, disk?: number, backup_slots?: number, network_speed?: number) => Promise<unknown>;
             get: () => Promise<unknown>;
+            edit: (cores?: number, mem?: number, disk?: number, backup_slots?: number, network_speed?: number) => Promise<unknown>;
         }>;
         rdns: (vmid: string, domain: string, ip_address: string) => Promise<unknown>;
         noVNC: (vmid: string) => Promise<unknown>;
@@ -38,9 +66,15 @@ export default class FireAPI {
         reset_password: (vmid: string) => Promise<unknown>;
         power: (vmid: string, mode: "start" | "stop" | "restart") => Promise<unknown>;
     };
+    /**
+     * @returns All available endpoints for your account
+     */
     account(): {
         requests: (offset?: number) => Promise<unknown>;
     };
+    /**
+     * @returns All available endpoints for your accounting
+     */
     accounting(): {
         invoices: () => Promise<unknown>;
         invoice: (invoice_id: string) => Promise<unknown>;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import wretch from "wretch";
 
+/**
+ * FireAPI - A simple wrapper for the 24Fire API
+ */
 export default class FireAPI {
+  /**
+   * The API key used for the requests
+   */
   private readonly apiKey: string;
 
+  /**
+   * @param apiKey The API key used for the requests
+   */
   constructor(apiKey: string) {
     this.apiKey = apiKey;
   }
 
+  /**
+   * @returns All available list endpoints
+   */
   public list() {
     const apiKey = this.apiKey;
+
+    /**
+     * @returns All available VMs
+     */
     async function vm() {
       return await wretch("https://live.fireapi.de/vm/list")
         .headers({
@@ -17,6 +33,10 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @returns All available hosts
+     */
     async function hosts() {
       return await wretch("https://live.fireapi.de/vm/list/hosts")
         .headers({
@@ -25,6 +45,10 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @returns All available operating systems
+     */
     async function os() {
       return await wretch("https://live.fireapi.de/vm/list/os")
         .headers({
@@ -33,6 +57,10 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @returns All available ISOs
+     */
     async function iso() {
       return await wretch("https://live.fireapi.de/vm/list/iso")
         .headers({
@@ -41,6 +69,10 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @returns All functions from anbove
+     */
     return {
       vm,
       hosts,
@@ -49,13 +81,31 @@ export default class FireAPI {
     };
   }
 
+  /**
+   * 
+   * @param vmid The ID of the VM
+   * @returns All available endpoints for DDoS protection
+   */
   public ddos(vmid: string) {
     const apiKey = this.apiKey;
 
+    /**
+     * @returns The current DDoS protection
+     */
     async function get() {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/ddos/`).headers({AUTHORIZATION: apiKey}).get().json();
     }
+
+    /**
+     * @param layer4 The layer 4 DDoS protection
+     * @param layer7 The layer 7 DDoS protection
+     * @param ip_address The IP address for the DDoS protection
+     * @returns The edited DDoS protection
+     */
     async function edit(layer4?: string, layer7?: string, ip_address?: string) {
+      /**
+       * If no parameter is given, return
+       */
       if (!layer4 && !layer7 && !ip_address) {
         return;
       }
@@ -70,21 +120,40 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       get,
       edit,
     };
   }
 
+  /**
+   * @param vmid The ID of the VM
+   * @returns All available endpoints for backup management
+   */
   public backup(vmid: string) {
     const apiKey = this.apiKey;
 
+    /**
+     * @returns All available backups
+     */
     async function get() {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/list`).headers({AUTHORIZATION: apiKey}).get().json();
     }
+
+    /**
+     * @returns The created backup
+     */
     async function create() {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/create`).headers({AUTHORIZATION: apiKey}).post().json();
     }
+
+    /**
+     * @param backup_id The ID of the backup
+     * @returns The status of the backup creation
+     */
     async function create_status(backup_id: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/create/status`)
         .headers({AUTHORIZATION: apiKey})
@@ -94,6 +163,11 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @param backup_id The ID of the backup
+     * @returns The restored backup
+     */
     async function restore(backup_id: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/restore`)
         .headers({AUTHORIZATION: apiKey})
@@ -103,6 +177,11 @@ export default class FireAPI {
         .post()
         .json();
     }
+
+    /**
+     * @param backup_id The ID of the backup
+     * @returns The status of the backup restoration
+     */
     async function restore_status(backup_id: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/restore/status`)
         .headers({AUTHORIZATION: apiKey})
@@ -112,6 +191,11 @@ export default class FireAPI {
         .get()
         .json();
     }
+
+    /**
+     * @param backup_id The ID of the backup
+     * @returns The JSON Response
+     */
     async function remove(backup_id: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/backup/delete`)
         .headers({AUTHORIZATION: apiKey})
@@ -122,6 +206,9 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       get,
       create,
@@ -132,9 +219,17 @@ export default class FireAPI {
     };
   }
 
+  /**
+   * @param vmid The ID of the VM
+   * @returns All available endpoints for ISO management
+   */
   public iso(vmid: string) {
     const apiKey = this.apiKey;
 
+    /**
+     * @param iso The ISO to attach
+     * @returns The JSON Response
+     */
     async function attach(iso: "3cx_debian_10." | "arch_linux_23_08" | "centos_7" | "centos_9" | "fedora_38" | "proxmox_8" | "proxmox_backup_server_3" | "rocky_linux_9" | "system_rescue_cd") {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/iso/`)
         .headers({AUTHORIZATION: apiKey})
@@ -144,19 +239,34 @@ export default class FireAPI {
         .put()
         .json();
     }
+
+    /**
+     * @returns The JSON Response
+     */
     async function detach() {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/iso/`).headers({AUTHORIZATION: apiKey}).delete().json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       attach,
       detach,
     };
   }
 
+  /**
+   * @returns All available endpoints for the VM
+   */
   public vm() {
     const apiKey = this.apiKey;
 
+    /**
+     * @param vmid The ID of the VM
+     * @param os? The OS to reinstall
+     * @returns The JSON Response
+     */
     async function reinstall(vmid: string, os?: "debian_11" | "debian_12" | "ubuntu_22_10" | "ubuntu_22_04" | "centos_7" | "windows_server_2019") {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/reinstall`)
         .headers({AUTHORIZATION: apiKey})
@@ -167,6 +277,17 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @param cores The amount of cores
+     * @param mem The amount of memory
+     * @param disk The amount of disk space
+     * @param os The OS to install
+     * @param hostsystem The host system
+     * @param ips? The amount of IPs
+     * @param backup_slots? The amount of backup slots
+     * @param network_speed? The network speed
+     * @returns The created VM
+     */
     async function create(cores: number, mem: number, disk: number, os: "debian_11" | "debian_12" | "ubuntu_22_10" | "ubuntu_22_04" | "centos_7" | "windows_server_2019", hostsystem: "nl_xeon" | "nl_ryzen", ips?: number, backup_slots?: number, network_speed?: number) {
       return await wretch(`https://live.fireapi.de/vm/create`)
         .body({
@@ -183,8 +304,30 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @returns All available endpoints for the VM config
+     */
     async function config(vmid: string) {
-      async function edit(vmid: string, cores?: number, mem?: number, disk?: number, backup_slots?: number, network_speed?: number) {
+      /**
+       * @returns The VM config
+       */
+      async function get() {
+        return await wretch(`https://live.fireapi.de/vm/${vmid}/config`).headers({AUTHORIZATION: apiKey}).get().json();
+      }
+
+      /**
+       * @param cores? The amount of cores
+       * @param mem? The amount of memory
+       * @param disk? The amount of disk space
+       * @param backup_slots? The amount of backup slots
+       * @param network_speed? The network speed
+       * @returns The edited VM config
+       */
+      async function edit(cores?: number, mem?: number, disk?: number, backup_slots?: number, network_speed?: number) {
+        /**
+         * If no parameter is given, return
+         */
         if (!cores && !mem && !disk && !backup_slots && !network_speed) {
           return;
         }
@@ -201,16 +344,22 @@ export default class FireAPI {
           .post()
           .json();
       }
-      async function get() {
-        return await wretch(`https://live.fireapi.de/vm/${vmid}/config`).headers({AUTHORIZATION: apiKey}).get().json();
-      }
 
+      /**
+       * @returns All functions from above
+       */
       return {
-        edit,
         get,
+        edit,
       };
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @param domain The domain for the rDNS
+     * @param ip_address The IP address for the rDNS
+     * @returns The JSON Response
+     */
     async function rdns(vmid: string, domain: string, ip_address: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/rdns`)
         .headers({AUTHORIZATION: apiKey})
@@ -222,26 +371,51 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @returns The JSON Response with link to the noVNC
+     */
     async function noVNC(vmid: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/novnc`).headers({AUTHORIZATION: apiKey}).post().json();
     }
-
+    
+    /**
+     * @param vmid The ID of the VM
+     * @returns The JSON Response
+     */
     async function remove(vmid: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/delete`).headers({AUTHORIZATION: apiKey}).delete().json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @returns The Status of the VM
+     */
     async function status(vmid: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/status`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @returns The Status of the VM installation
+     */
     async function status_installation(vmid: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/status/installation`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @returns The JSON Response with the new password
+     */
     async function reset_password(vmid: string) {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/password-reset`).headers({AUTHORIZATION: apiKey}).post().json();
     }
 
+    /**
+     * @param vmid The ID of the VM
+     * @param mode The mode to power the VM
+     * @returns The JSON Response
+     */
     async function power(vmid: string, mode: "start" | "stop" | "restart") {
       return await wretch(`https://live.fireapi.de/vm/${vmid}/power`)
         .headers({AUTHORIZATION: apiKey})
@@ -252,6 +426,9 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       reinstall,
       create,
@@ -266,9 +443,15 @@ export default class FireAPI {
     };
   }
 
+  /**
+   * @returns All available endpoints for your account
+   */
   public account() {
     const apiKey = this.apiKey;
 
+    /**
+     * @returns All requests
+     */
     async function requests(offset?: number) {
       return await wretch(`https://live.fireapi.de/account/requests`)
         .headers({AUTHORIZATION: apiKey})
@@ -279,30 +462,52 @@ export default class FireAPI {
         .json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       requests,
     };
   }
 
+  /**
+   * @returns All available endpoints for your accounting
+   */
   public accounting() {
     const apiKey = this.apiKey;
 
+    /**
+     * @returns All invoices
+     */
     async function invoices() {
       return await wretch(`https://live.fireapi.de/accounting/invoices`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @param invoice_id The ID of the invoice
+     * @returns The invoice
+     */
     async function invoice(invoice_id: string) {
       return await wretch(`https://live.fireapi.de/accounting/invoices/${invoice_id}`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @returns The current invoice
+     */
     async function invoice_current() {
       return await wretch(`https://live.fireapi.de/accounting/invoices/current`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @returns All pricings
+     */
     async function pricing() {
       return await wretch(`https://live.fireapi.de/accounting/pricings`).headers({AUTHORIZATION: apiKey}).get().json();
     }
 
+    /**
+     * @returns All functions from above
+     */
     return {
       invoices,
       invoice,
